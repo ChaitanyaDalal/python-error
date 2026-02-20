e6a242bf9d26547d1096dc3e754c11c13ea83924
Sudo_Chaitanya <chaitanyadalal100@gmail.com>
2026-02-20 09:03:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,27 +2,14 @@ class Student:
     def __init__(self, name, grades=[]):
         self.name = name
         self.grades = grades
-
     def add_grade(self, grade):
         self.grades.append(grade)
-
     def average(self):
         total = 0
         for g in self.grades:
             total += g
         return total / len(self.grades)
-
 s1 = Student("Alice")
 s1.add_grade(90)
 s1.add_grade("85")
-
 print("Average grade:", s1.average())
-
-
-
-
-
-
-
-
-
